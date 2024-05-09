5f9999a53d90e16b0da3620352b02256243a54a0
VitoMarino <marino.vito2001@gmail.com>
2024-05-09 16:31:53 +0200

    form

--- script.js
+++ script.js
@@ -5,3 +5,18 @@
 // il prezzo del biglietto è definito in base ai km (0.267113 € al km)
 // va applicato uno sconto del 24.552% per gli under 21
 // va applicato uno sconto del 37.893% per gli over 63.
+
+
+
+let bottone = document.querySelector('button');
+
+let prezzoAlKm = 0.267113;
+let scontoUno = 24.552;
+let scontoDue = 37.893;
+
+bottone.addEventListener('click', function() {
+    let kmDaPercorrere = document.querySelector('label:nth-child(1) > div');
+    let age = document.querySelector('label:nth-child(2) > div');
+    console.log(kmDaPercorrere.value, age.value);
+})
+
