3211890ee73003e82f7beb4512de0ead5050652e
VitoMarino <marino.vito2001@gmail.com>
2024-05-09 14:31:27 +0200

    start

---
+++ script.js
@@ -0,0 +1,7 @@
+// Scrivere un programma che chieda all’utente:
+// Il numero di chilometri da percorrere
+// Età del passeggero 
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// il prezzo del biglietto è definito in base ai km (0.267113 € al km)
+// va applicato uno sconto del 24.552% per gli under 21
+// va applicato uno sconto del 37.893% per gli over 63.
